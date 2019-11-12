7f083e0b19e6f7eb26ba610185d39d4347f879fe
PastuD <PastuD@7W304-05>
2019-11-12 20:08:42 +0300

    Lab 4 done

--- Lab4/Program.cs
+++ Lab4/Program.cs
@@ -10,6 +10,73 @@ namespace Lab4
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Input first num");
+            int x = int.Parse(Console.ReadLine);
+            Console.WriteLine("Input secon num");
+            int y = int.Parse(Console.ReadLine);
+
+            int greater = Utils.Greater(x, y);
+            Console.WriteLine("The greater value is:" + greater);
+
+            Console.WriteLine("Befor swap:" + x + ", " + y);
+            Utils.Swap(ref x, ref y);
+            Console.WriteLine("After swap:" + x + "," + y);
+
+            //3 задача
+            bool fact = Utils.Factorial(x, out f);
+            if (ok)
+            {
+                Console.WriteLine("Factorial x(" + x + ")=" + f);
+            }
+            else
+                Console.WriteLine("Cannot compute this factorial");
+        }
+    }
+
+    class Utils
+    {
+        public static int Greater(int a, int b)
+        {
+            if (a > b)
+            {
+                return a;
+            }
+            else
+                return b;
+
+        }
+
+        public static void Swap(int a, int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+
+        public static bool Factorial(int n, out int answer)
+        {
+            int k;
+            int f = 1;
+            bool ok = true;
+
+            if (ok < 0) ok = false;
+            try
+            {
+                checked
+                {
+                    for (k = 2; k <= n; ++k)
+                    {
+                        f *= k;
+                    }
+                }
+            }catch(Exception e)
+            {
+                f = 0;
+                ok = false;
+                //Console.WriteLine(e);
+            }
+            answer = f;
+            return ok;
         }
     }
 }
